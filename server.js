f6301951effc63de1385e5e9b03cce35903b70fb
Ivan Gerard & Ryan Andhaka <pair+ivangerard.ryanandhaka@hacktiv8.com>
2016-09-20 16:19:14 +0700

    Update

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set('view engine', 'jade');
 
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/purexpress');
